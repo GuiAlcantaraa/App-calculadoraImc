1b82406ddc163d033344232dd2c6da399b376326
Guilherme <redshotguii@outlook.com]>
2022-03-31 18:04:32 -0300

    First commmit

--- App.jsx
+++ App.jsx
@@ -1,10 +1,9 @@
 import Home from './src/screens/Home/Index';
 
 export default function App() {
-
   return (
     <Home />
   )
-
-
 }
+
+
--- src/screens/Home/Index.jsx
+++ src/screens/Home/Index.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Button } from '../../Components/Button/Index';
 import { Container, InputText, Title, ResultText } from './styles'
-import { Text } from 'react-native';
+
 
 export default function Home() {
 
@@ -12,16 +12,32 @@ export default function Home() {
     const [resultText, setResultText] = useState("")
 
     function handleCalculateImc() {
-        let calculateImc = (weight / (height * height)).toFixed(2)
-        setResultImc(calculateImc)
+       const calculateImc = (weight / (height * height)).toFixed(2)
+        if(height !== 0 && weight !==0){
+            setResultText("Informe altura e peso")
+        }
 
-        if (resultImc < 18.5) setResultText("Muito abaixo do peso")
-        else if (resultImc > 18.5 && resultImc < 24.9) setResultText("Seu peso esta normal")
-        else if (resultImc >= 25 && resultImc <= 29.9) setResultText("Sobrepeso")
-        else if (resultImc >= 30 && resultImc <= 39.9) setResultText("Obesidade")
-        else if (resultImc > 40) setResultText("Obesidade grave")
+        if (calculateImc < 18.5){
+            setResultImc(calculateImc)
+            setResultText("Muito abaixo do peso") 
+        }
+        else if (calculateImc > 18.5 && calculateImc < 24.9){
+            setResultImc(calculateImc)
+            setResultText("Seu peso esta normal")
+        }
+        else if (calculateImc >= 25 && calculateImc <= 29.9){ 
+            setResultImc(calculateImc)
+            setResultText("Sobrepeso")
+        }
+        else if (calculateImc >= 30 && calculateImc <= 39.9) {
+            setResultImc(calculateImc)
+            setResultText("Obesidade")
+        }
+        else if (calculateImc > 40){
+            setResultImc(calculateImc)
+            setResultText("Obesidade grave")
+        }
     }
-
     return (
         <Container>
 
@@ -34,13 +50,13 @@ export default function Home() {
                 keyboardType="numeric"
                 onChangeText={setHeight}
             />
-
             <InputText
                 placeholder='Digite o peso'
                 placeholderTextColor="#999"
                 keyboardType="numeric"
                 onChangeText={setWeight}
             />
+            <Icon name="users" size={60} color="#900"/>
 
             <Button onPress={handleCalculateImc} />
 
